7af363a92c19e630088f2648f2d7709cc6a15250
waite <waite1x@outlook.com>
2024-03-18 14:09:23 +0800

    fix: fix

--- src/mappings/asset_metadata.ts
+++ src/mappings/asset_metadata.ts
@@ -1,6 +1,12 @@
 import { DataHandlerContext } from "@subsquid/evm-processor";
 import { Store } from "@subsquid/typeorm-store";
-import fetch from "node-fetch"
+
+const _importDynamic = new Function('modulePath', 'return import(modulePath)')
+
+async function fetch(...args: any) {
+  const { default: fetch } = await _importDynamic('node-fetch')
+  return fetch(...args)
+}
 
 export type AssetMetaData = {
   type?: string;
