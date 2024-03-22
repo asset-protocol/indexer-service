f062c1460e71195d4068b7c77231c6452d146f8f
waite <waite1x@outlook.com>
2024-03-22 12:31:56 +0800

    fix

--- src/server-extension/resolvers/blob-resolver.ts
+++ src/server-extension/resolvers/blob-resolver.ts
@@ -1,9 +1,8 @@
 import { Arg, Query, Resolver } from "type-graphql";
 import { EntityManager } from "typeorm";
 import { createLogger } from '@subsquid/logger'
-// import { fetch } from '../../common/fetch';
+import { fetch } from '../../common/fetch';
 
-const _importDynamic = new Function('modulePath', 'return import(modulePath)')
 @Resolver()
 export class BlobResolver {
   constructor(private tx: () => Promise<EntityManager>) { }
@@ -14,19 +13,19 @@ export class BlobResolver {
   ): Promise<string> {
     const logger = createLogger('sqd:graphql-server:blob-resolver');
     return new Promise((resolve, reject) => {
-      fetch(url).then((res: any) => {
+      fetch(url).then((res) => {
         if (res.ok) {
           logger.info("fetched blob size: " + res.size)
           if (res.size > 200 * 1024 * 1024) {
             throw new Error("body is limmited to 200mb")
           }
-          return res.buffer();
+          return res.arrayBuffer();
         } else {
           throw new Error(res.statusText);
         }
       }).then((blob) => {
-        const b64 = blob.toString('binary');
-        resolve(b64);
+        const bb = Buffer.from(blob).toString("binary");
+        resolve(bb);
       }).catch((e: any) => {
         logger.error(`fetch ${url} blob failed: ${e.message}`);
         resolve("");
