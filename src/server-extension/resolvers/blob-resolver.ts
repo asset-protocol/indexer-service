b650ce47c71a2222af83f6d4900fd341544c93fc
waite <waite1x@outlook.com>
2024-03-22 09:13:25 +0800

    fix

--- src/server-extension/resolvers/blob-resolver.ts
+++ src/server-extension/resolvers/blob-resolver.ts
@@ -1,7 +1,7 @@
 import { Arg, Query, Resolver } from "type-graphql";
 import { EntityManager } from "typeorm";
 import { createLogger } from '@subsquid/logger'
-import { fetch } from '../../common/fetch';
+// import { fetch } from '../../common/fetch';
 
 const _importDynamic = new Function('modulePath', 'return import(modulePath)')
 @Resolver()
@@ -20,14 +20,13 @@ export class BlobResolver {
           if (res.size > 200 * 1024 * 1024) {
             throw new Error("body is limmited to 200mb")
           }
-          return res.arrayBuffer();
+          return res.buffer();
         } else {
           throw new Error(res.statusText);
         }
-      }).then((blob: any) => {
-        const fr = new TextDecoder();
-        const str = fr.decode(blob);
-        resolve(str);
+      }).then((blob) => {
+        const b64 = blob.toString('base64');
+        resolve(b64);
       }).catch((e: any) => {
         logger.error(`fetch ${url} blob failed: ${e.message}`);
         resolve("");
