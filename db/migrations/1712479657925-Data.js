187c6269c6d42691a0731e8d2ac752935cd4ed32
waite1x <waite1x@outlook.com>
2024-04-07 16:48:28 +0800

    fix: Fix db schema

---
+++ db/migrations/1712479657925-Data.js
@@ -0,0 +1,53 @@
+module.exports = class Data1712479657925 {
+    name = 'Data1712479657925'
+
+    async up(db) {
+        await db.query(`CREATE TABLE "asset_metadata_history" ("id" character varying NOT NULL, "timestamp" numeric, "content_uri" text, "metadata" text, "asset_id" character varying, CONSTRAINT "PK_94cab17983a7cc063b3c9ffb88b" PRIMARY KEY ("id"))`)
+        await db.query(`CREATE INDEX "IDX_8a6618115b581777839b5c5226" ON "asset_metadata_history" ("asset_id") `)
+        await db.query(`CREATE INDEX "IDX_a5c56b22e8b74e45f5dcf76090" ON "asset_metadata_history" ("timestamp") `)
+        await db.query(`CREATE TABLE "collector" ("id" character varying NOT NULL, "collector" text, "token_id" numeric, "collect_module" text, "collect_module_data" text, "timestamp" numeric, "asset_id" character varying, CONSTRAINT "PK_0d93071208ac3e8dd49506d903f" PRIMARY KEY ("id"))`)
+        await db.query(`CREATE INDEX "IDX_e235ae1707cf7e637e9353a51b" ON "collector" ("asset_id") `)
+        await db.query(`CREATE INDEX "IDX_a8122903753f245d5aa3f2c7dd" ON "collector" ("collector") `)
+        await db.query(`CREATE INDEX "IDX_11049fc3d3cabc159ee2fd3bd2" ON "collector" ("timestamp") `)
+        await db.query(`CREATE TABLE "asset" ("id" character varying NOT NULL, "asset_id" numeric, "hub" text, "publisher" text, "content_uri" text, "timestamp" numeric, "hash" text, "last_updated_at" numeric, "query1" text, "query2" text, "name" text, "type" text, "image" text, "description" text, "tags" text, "extra" text, "metadata" text, "content" text, "collect_module" text, "collect_module_init_data" text, "collect_nft" text, "collect_count" numeric, "gated_module" text, "gated_module_init_data" text, CONSTRAINT "PK_1209d107fe21482beaea51b745e" PRIMARY KEY ("id"))`)
+        await db.query(`CREATE INDEX "IDX_2a48e81afa7729ed31c2c7b18e" ON "asset" ("asset_id") `)
+        await db.query(`CREATE INDEX "IDX_d439f59d73cbe9ba229c136375" ON "asset" ("hub") `)
+        await db.query(`CREATE INDEX "IDX_da5414205913c272959745b45f" ON "asset" ("publisher") `)
+        await db.query(`CREATE INDEX "IDX_b08d89b57c60050b62d774665f" ON "asset" ("timestamp") `)
+        await db.query(`CREATE INDEX "IDX_1e7c82e366bef4ec6c2ba46ca2" ON "asset" ("last_updated_at") `)
+        await db.query(`CREATE INDEX "IDX_0b784bbba208ae9850eb203dd0" ON "asset" ("query1") `)
+        await db.query(`CREATE INDEX "IDX_c829d56b31e8e2730cea6029f8" ON "asset" ("query2") `)
+        await db.query(`CREATE TABLE "contract_info" ("id" character varying NOT NULL, "name" text, "symbol" text, "description" text, "image" text, "banner_image" text, "featured_image" text, "external_link" text, "collaborators" text, CONSTRAINT "PK_de5fc90e42a90a8a9d5233e3f2e" PRIMARY KEY ("id"))`)
+        await db.query(`CREATE TABLE "asset_hub" ("id" character varying NOT NULL, "admin" text, "name" text, "token_collect_module" text, "fee_collect_module" text, "nft_gated_module" text, "create_asset_module" text, "timestamp" numeric, "implementation" text, "version" text, "hash" text, CONSTRAINT "PK_99771a62f48504f4154931b5819" PRIMARY KEY ("id"))`)
+        await db.query(`CREATE UNIQUE INDEX "IDX_02047cd2770a4aca3144fdc342" ON "asset_hub" ("name") `)
+        await db.query(`CREATE INDEX "IDX_de83b6baa9c66ff8a598cddb4b" ON "asset_hub" ("timestamp") `)
+        await db.query(`CREATE TABLE "hub_manager" ("id" character varying NOT NULL, "timestamp" numeric, "global_module" text, CONSTRAINT "PK_9c07121c4e2c56ed6dc59bfd7d1" PRIMARY KEY ("id"))`)
+        await db.query(`ALTER TABLE "asset_metadata_history" ADD CONSTRAINT "FK_8a6618115b581777839b5c52260" FOREIGN KEY ("asset_id") REFERENCES "asset"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`)
+        await db.query(`ALTER TABLE "collector" ADD CONSTRAINT "FK_e235ae1707cf7e637e9353a51b7" FOREIGN KEY ("asset_id") REFERENCES "asset"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`)
+    }
+
+    async down(db) {
+        await db.query(`DROP TABLE "asset_metadata_history"`)
+        await db.query(`DROP INDEX "public"."IDX_8a6618115b581777839b5c5226"`)
+        await db.query(`DROP INDEX "public"."IDX_a5c56b22e8b74e45f5dcf76090"`)
+        await db.query(`DROP TABLE "collector"`)
+        await db.query(`DROP INDEX "public"."IDX_e235ae1707cf7e637e9353a51b"`)
+        await db.query(`DROP INDEX "public"."IDX_a8122903753f245d5aa3f2c7dd"`)
+        await db.query(`DROP INDEX "public"."IDX_11049fc3d3cabc159ee2fd3bd2"`)
+        await db.query(`DROP TABLE "asset"`)
+        await db.query(`DROP INDEX "public"."IDX_2a48e81afa7729ed31c2c7b18e"`)
+        await db.query(`DROP INDEX "public"."IDX_d439f59d73cbe9ba229c136375"`)
+        await db.query(`DROP INDEX "public"."IDX_da5414205913c272959745b45f"`)
+        await db.query(`DROP INDEX "public"."IDX_b08d89b57c60050b62d774665f"`)
+        await db.query(`DROP INDEX "public"."IDX_1e7c82e366bef4ec6c2ba46ca2"`)
+        await db.query(`DROP INDEX "public"."IDX_0b784bbba208ae9850eb203dd0"`)
+        await db.query(`DROP INDEX "public"."IDX_c829d56b31e8e2730cea6029f8"`)
+        await db.query(`DROP TABLE "contract_info"`)
+        await db.query(`DROP TABLE "asset_hub"`)
+        await db.query(`DROP INDEX "public"."IDX_02047cd2770a4aca3144fdc342"`)
+        await db.query(`DROP INDEX "public"."IDX_de83b6baa9c66ff8a598cddb4b"`)
+        await db.query(`DROP TABLE "hub_manager"`)
+        await db.query(`ALTER TABLE "asset_metadata_history" DROP CONSTRAINT "FK_8a6618115b581777839b5c52260"`)
+        await db.query(`ALTER TABLE "collector" DROP CONSTRAINT "FK_e235ae1707cf7e637e9353a51b7"`)
+    }
+}
