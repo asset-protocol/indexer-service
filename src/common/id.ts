5d1663a8ead81d73e0515e94e94278daf06bf372
waite <waite1x@outlook.com>
2024-06-20 10:40:10 +0800

    fix: Fix error

--- src/common/id.ts
+++ src/common/id.ts
@@ -1,4 +1,4 @@
-const { customAlphabet } = require('nanoid');
+import { customAlphabet } from 'nanoid';
 const alphabet =
   '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 const nanoid = customAlphabet(alphabet, 12);
