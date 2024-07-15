e445bd8d0196ae06e8889a60b5640bca699dc7f5
waite <waite1x@outlook.com>
2024-07-15 14:22:42 +0800

    fix: timeout

--- src/mappings/asset_metadata.ts
+++ src/mappings/asset_metadata.ts
@@ -1,4 +1,4 @@
-import { Logger } from "@subsquid/logger";
+import { Logger } from '@subsquid/logger';
 import { fetch } from '../common/fetch';
 
 export type AssetMetaData = {
@@ -12,41 +12,47 @@ export type AssetMetaData = {
   query2?: string;
   attributes?: unknown;
   timestamp?: string;
-}
+};
 
-const ipfsEndpoint = process.env.IPFS_GATEWAY + "/";
+const ipfsEndpoint = process.env.IPFS_GATEWAY + '/';
 
-export async function fetchMetadata<T>(ctx: { log: Logger }, uri: string): Promise<T | undefined> {
+export async function fetchMetadata<T>(
+  ctx: { log: Logger },
+  uri: string
+): Promise<T | undefined> {
   if (!uri) {
     return;
   }
-  if (uri.trimStart().startsWith("{")) { //JSON
+  if (uri.trimStart().startsWith('{')) {
+    //JSON
     try {
-      const obj = JSON.parse(uri)
+      const obj = JSON.parse(uri);
       if (obj.name !== undefined) {
-        return obj
+        return obj;
       }
-    } catch { }
+    } catch {}
   }
   try {
     uri = parseUri(uri);
-    ctx.log.info("fetching metadata: " + uri)
-    const data = await fetch(uri).then(r => r.json());
+    ctx.log.info('fetching metadata: ' + uri);
+    const data = await fetch(uri, {
+      timeout: 60000,
+    }).then((r) => r.json());
     return data as T;
   } catch (e: any) {
-    ctx.log.warn("fetch metadata error: " + e?.message);
+    ctx.log.warn('fetch metadata error: ' + e?.message);
   }
 }
 
 function parseUri(uri: string): string {
-  if (uri.startsWith("http://") || uri.startsWith("https://")) {
+  if (uri.startsWith('http://') || uri.startsWith('https://')) {
     return uri;
   }
-  if (uri.startsWith("ipfs://")) {
+  if (uri.startsWith('ipfs://')) {
     return ipfsEndpoint + uri.slice(7);
   }
-  if (uri.startsWith("ar://")) {
+  if (uri.startsWith('ar://')) {
     return `${process.env.ARWAVE_GATEWAY}/${uri.slice(5)}`;
   }
-  return ""
+  return '';
 }
